846c626d2b09b0eed3bfde532a07817d2b06ee21
AlexRS90 <aramosbengy@gmail.com>
2022-01-24 18:37:36 -0600

    Refactor can_use_services method

--- person.rb
+++ person.rb
@@ -2,22 +2,26 @@ class Person
   attr_accessor :name, :age
   attr_reader :id
 
-  def inicialize( name = "Unknown", age, parent_permission = true )
+  def initialize(name = "Unknown", age, parent_permission: true)
     @id = Random.rand(1..500)
     @name = name
     @age = age
+    @parent_permission = parent_permission
   end
 
-  def can_use_services?(person)
-    if person.is_of_age? || person.parent_permission
-      true
+  def can_use_services?
+    age_of_person = is_of_age?
+    if age_of_person || @parent_permission
+      return true
+    else
+      return false
     end  
   end
 
   private
 
-  def is_of_age?(person)
-    if person.age >= 18
+  def is_of_age?
+    if @age >= 18
       true
     else 
       false
