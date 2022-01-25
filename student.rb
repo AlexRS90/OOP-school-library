f5523541f2d6dce93e0627618c79e6cc0bbb57a3
AlexRS90 <aramosbengy@gmail.com>
2022-01-24 21:18:34 -0600

    Arrange initialize parameters as in Person class

--- student.rb
+++ student.rb
@@ -1,12 +1,12 @@
 require './person'
 
 class Student < Person
-  def initialize(name, age, class_room)
-    super(name, age)
+  def initialize(age, name, class_room, parent_permission = true)
+    super(age, name, parent_permission)
     @class_room = class_room
   end
 
   def play_hooky
-    '¯\(ツ)/¯'
+    "¯\(ツ)/¯"
   end
 end
