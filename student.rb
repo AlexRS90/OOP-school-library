a528d3cf795885f2065e92fd4e4090e18cfc6a6a
AlexRS90 <aramosbengy@gmail.com>
2022-01-24 21:37:42 -0600

    Fix linters

--- student.rb
+++ student.rb
@@ -1,7 +1,7 @@
 require './person'
 
 class Student < Person
-  def initialize(age, name, class_room, parent_permission: true)
+  def initialize(age, name, class_room)
     super(age, name, parent_permission: true)
     @class_room = class_room
   end
@@ -10,3 +10,6 @@ class Student < Person
     "¯\(ツ)/¯"
   end
 end
+
+janeth = Student.new(13, 'Janeth', "CC")
+p janeth.play_hooky
