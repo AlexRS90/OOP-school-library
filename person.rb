625a4ff960c6f983107c7451d092b8eda15a5914
AlexRS90 <aramosbengy@gmail.com>
2022-01-26 23:49:37 -0600

    Add relationships many to many with Book through Rental

--- person.rb
+++ person.rb
@@ -2,7 +2,7 @@ require './corrector'
 
 class Person
   attr_accessor :name, :age
-  attr_reader :id
+  attr_reader :id, :rental
 
   def initialize(age, name = 'Unknown', parent_permission: true)
     @id = Random.rand(1..500)
@@ -10,6 +10,7 @@ class Person
     @age = age
     @parent_permission = parent_permission
     @check_name = Corrector.new
+    @rental = []
   end
 
   def validate_name
