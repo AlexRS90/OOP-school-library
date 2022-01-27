2a165ae9bafcdce912d22b7aecaa1453c5c6e133
AlexRS90 <aramosbengy@gmail.com>
2022-01-27 09:47:32 -0600

    Add add_rental method

--- book.rb
+++ book.rb
@@ -1,10 +1,14 @@
 class Book
-  attr_accessor :title, :author
-  attr_reader :rental
+  attr_accessor :title, :author, :rental
 
   def initialize(title, author)
     @title = title
     @author = author
     @rental = []
+  end
+
+  def add_rental(book)
+    @rental.push(book)
+    rental.book = self
   end
 end
