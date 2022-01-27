ce5d13611cc265d297909b31a709bf3ad545e9de
AlexRS90 <aramosbengy@gmail.com>
2022-01-26 23:48:26 -0600

    Create Person object

--- rental.rb
+++ rental.rb
@@ -1,7 +1,14 @@
 class Rental
   attr_accessor :date
+  attr_reader :person, :book
 
-  def initialize(date)
+  def initialize(date, person, book)
     @date = date
+
+    @person = person
+    person.rental << self
+
+    @book = book
+    book.rental << self
   end
 end
