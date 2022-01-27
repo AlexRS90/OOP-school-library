b66821ad19340248e5e081c07dde65106cdf6600
AlexRS90 <aramosbengy@gmail.com>
2022-01-26 22:44:47 -0600

    Add has many relationships with classroom

--- student.rb
+++ student.rb
@@ -1,6 +1,9 @@
 require './person'
+require './classroom'
 
 class Student < Person
+  attr_accessor :classroom
+
   def initialize(age, name, class_room, parent_permission: true)
     super(age, name, parent_permission: parent_permission)
     @class_room = class_room
@@ -10,3 +13,16 @@ class Student < Person
     "¯\(ツ)/¯"
   end
 end
+
+alex = Student.new(31, "Alex", "OOP")
+class_room_1 = Classroom.new("Classroom A: Math")
+p class_room_1.student
+class_room_1.add_student(alex)
+
+
+p class_room_1.label
+p class_room_1.student
+p alex.name
+
+alex.classroom = class_room_1
+p alex.classroom.label
