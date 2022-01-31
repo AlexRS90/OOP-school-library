638884790e5cfac8dba6c6a63214e6f260e3136f
Carlos E. Feliz <cfelizmedina@gmail.com>
2022-01-31 16:25:52 -0400

    add options for users

--- main.rb
+++ main.rb
@@ -1,8 +1,57 @@
 #!/usr/bin/env ruby
 require_relative 'app'
+class App 
+  include Library
+  def welcome
+    puts "\nWelcome to School Library App!"
+    loop do
+      menu
+
+      answer = gets.chomp
+
+      break if answer == '7'
+
+      option answer
+    end
+  end
+
+  def option(answer)
+    case answer
+    when 1
+      show_all_books
+    when 2
+      show_people
+    when 3
+      add_new_person
+    when 4
+      add_new_book
+    when 5
+      new_rental
+    when 6
+      show_rentals
+    else
+      puts "\nThanks for your visit, have a great day!"
+      abort
+    end
+  end
+
+  def menu
+    puts "\nPlease choose an option by entering a number:"
+    puts '1 - List all books'
+    puts '2 - List all people'
+    puts '3 - Create a person'
+    puts '4 - Create a book'
+    puts '5 - Create a rental'
+    puts '6 - List all rentals for a given person ID'
+    puts '7 - Exit'
+    answer = gets.chomp.to_i
+    option(answer)
+  end
+end
+
 
 def main
-  app = Library.new
+  app = App.new
   app.welcome
 end
 
