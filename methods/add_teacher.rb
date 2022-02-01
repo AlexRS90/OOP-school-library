6a07c8e12e85b36fe9b92a33835d87943775b1c5
AlexRS90 <aramosbengy@gmail.com>
2022-02-01 11:22:27 -0600

    Add methos to follow SOLID rules

--- methods/add_teacher.rb
+++ methods/add_teacher.rb
@@ -4,13 +4,18 @@ require_relative './people_memory'
 class AddTeacher
   include PeopleMemory
 
-  def add
+  def teacher_details
     print "\nAge: "
     age = gets.chomp.to_i
     print 'Name: '
     name = gets.chomp
     print 'Specialization: '
     specialization = gets.chomp
+    [age, name, specialization]
+  end
+
+  def add
+    age, name, specialization = teacher_details
     teacher = Teacher.new(age, name, specialization)
     add_person(teacher)
 
