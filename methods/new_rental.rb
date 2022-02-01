29c59463843921203de7bb464b9ab34e50f6861a
AlexRS90 <aramosbengy@gmail.com>
2022-02-01 11:34:29 -0600

    Separate methos to follow SOLID rules

--- methods/new_rental.rb
+++ methods/new_rental.rb
@@ -13,7 +13,7 @@ class AddRental
     @people = show_people
   end
 
-  def add
+  def rental_details
     if @people.empty? || @books.empty?
       puts 'Add a book or person before continue, thank you!'
     else
@@ -29,6 +29,13 @@ class AddRental
       puts 'YYYY/MM/DD'
       print 'Add a date in the given format: '
       date = gets.chomp
+      [book_selected, person_selected, date]
+    end
+  end
+
+  def add
+    book_selected, person_selected, date = rental_details
+    if book_selected && person_selected != nil
       rental = Rental.new(date, @people[person_selected], @books[book_selected])
       add_rental(rental)
       puts "\nRental created successfully\nEnjoy your book =D"
