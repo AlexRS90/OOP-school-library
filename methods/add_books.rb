df397c0d5dc46837fd3eb31dc7b5b204f290b784
AlexRS90 <aramosbengy@gmail.com>
2022-02-01 11:22:01 -0600

    Add methos to follow SOLID rules

--- methods/add_books.rb
+++ methods/add_books.rb
@@ -4,11 +4,16 @@ require_relative './books_memory'
 class AddBooks
   include BooksMemory
 
-  def add
+  def book_details
     print "\nTitle: "
     title = gets.chomp.capitalize
     print 'Author: '
     author = gets.chomp.capitalize
+    [title, author]
+  end
+
+  def add
+    title, author = book_details
     book_name = Book.new(title, author)
     add_books(book_name)
     puts 'Book created successfully'
