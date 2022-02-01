86be8f4d19d8c3b5b3eccf188448fb4fa9ba7144
AlexRS90 <aramosbengy@gmail.com>
2022-01-31 18:19:53 -0600

    Add showRentals class

---
+++ methods/showRentals.rb
@@ -0,0 +1,21 @@
+class Display_rentals
+  def show
+    if $rentals.empty?
+      puts "\nAdd a new rental before continue =D"
+    else
+      puts "\nSelect a person by ID from the list: "
+      $people.each do |person|
+        print "#{$people.find_index(person) + 1}) "
+        print "[#{person.class}] ID: #{person.id} Name: #{person.name}\n"
+      end
+      person_selected = gets.chomp.to_i
+      puts 'Rentals: '
+      $rentals.each do |rental|
+        if rental.person.id == person_selected
+          puts "Date: #{rental.date}, Book: #{rental.book.title}, Author: #{rental.book.title}"
+        end
+      end
+    end
+    $app.menu
+  end
+end
